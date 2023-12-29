eb6a351963b868e0f057d968c7c34cab4e98bf3d
KostiaTrepyk <137499840+KostiaTrepyk@users.noreply.github.com>
2023-12-29 23:15:02 +0100

    Fixed uploadFile helper

--- src/helpers/uploadFile.ts
+++ src/helpers/uploadFile.ts
@@ -1,29 +1,46 @@
 import { cookieKeys } from "types/cookie";
 import { getCookieValue } from "./cookie";
-import { UploadFileBody } from "services/filesApi";
+import { type UploadFileBody } from "services/filesApi";
 
+/**
+ * Initiates the file upload process by creating a dynamic file input element,
+ * triggering file selection, and using the provided function to start the upload.
+ *
+ * @param uploadFile - Function to initiate the file upload process.
+ * @param folderId - Optional folder ID where the file should be uploaded.
+ * @param storageId - The storage ID where the file should be uploaded.
+ */
 export function uploadFile({
 	uploadFile,
-	currentFolderId,
+	folderId,
+	storageId,
 }: {
 	uploadFile: (data: UploadFileBody) => void;
-	currentFolderId: number;
+	folderId?: number;
+	storageId: number;
 }) {
+	// Create a dynamic file input element
 	const input = document.createElement("input");
 	input.setAttribute("type", "file");
+
+	// Append the input element to the document body and trigger a click to open the file selection dialog
 	document.body.appendChild(input);
 	input.click();
+
+	// Set up an event listener for file selection
 	input.onchange = () => {
+		// Check if files were selected
 		if (!input.files) return;
 
-		const file = input.files[0];
-
+		// Retrieve the selected file and initiate the file upload
 		uploadFile({
-			token: getCookieValue(cookieKeys.TOKEN),
-			folderId: currentFolderId,
-			file: file,
+			token: getCookieValue(cookieKeys.TOKEN), // Get authentication token from cookies
+			folderId,
+			storageId,
+			file: input.files[0],
 		});
 	};
 
+	// Remove the dynamically created input element from the DOM after file selection
 	input.parentNode?.removeChild(input);
 }
