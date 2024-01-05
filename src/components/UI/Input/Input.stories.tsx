a22ac39ca32443863a264a9226c67afbc4acdfda
KostiaTrepyk <137499840+KostiaTrepyk@users.noreply.github.com>
2024-01-05 16:37:51 +0100

    (UI) Improved Input and added story

---
+++ src/components/UI/Input/Input.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Input from "./Input";
+
+
+const meta: Meta<typeof Input> = {
+	title: "Input",
+	component: Input,
+	parameters: {},
+	tags: ["autodocs"],
+};
+
+export default meta;
+
+export const Default: Story = {};
+
+export const WithLabel: Story = { args: { label: { text: "Label" } } };
+
+export const Disabled: Story = {
+	args: {
+		label: { text: "Label" },
+		input: { disabled: true },
+	},
+};
+
+export const Required: Story = {
+	args: {
+		label: { text: "Label" },
+		input: { required: true },
+	},
+};
+
+export const FullWidth: Story = {
+	args: {
+		fullWidth: true,
+	},
+};
+
+type Story = StoryObj<typeof meta>;
---
+++ src/components/UI/Input/Input.tsx
@@ -0,0 +1,63 @@
+import { forwardRef, useId } from "react";
+import { twMerge } from 'tailwind-merge'
+
+interface InputProps {
+	fullWidth?: boolean;
+
+	wrapper?: {
+		className?: string;
+	};
+	label?: {
+		text: string;
+		className?: string;
+	};
+	input?: {
+		className?: string;
+	} & React.InputHTMLAttributes<HTMLInputElement>;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+	({ fullWidth = false, wrapper, label, input }, ref) => {
+		const inputId = useId();
+
+		return (
+			<div
+				{...wrapper}
+				className={twMerge(
+					"flex w-fit flex-col gap-1",
+					fullWidth && "w-full",
+					wrapper?.className
+				)}
+			>
+				{label?.text && (
+					<label
+						htmlFor={inputId}
+						className={twMerge(
+							"w-full px-0.5 text-neutral-900 transition-colors duration-500",
+							input?.disabled && "text-neutral-400",
+							label.className
+						)}
+					>
+						{input?.required && (
+							<span className="text-red-600">*&nbsp;</span>
+						)}
+						{label.text}
+					</label>
+				)}
+				<input
+					{...input}
+					id={inputId}
+					className={twMerge(
+						"w-full rounded border border-neutral-300 px-2 py-1 outline-none transition duration-500 hover:bg-neutral-100 focus:border-neutral-500 focus:bg-neutral-200",
+						input?.value && "invalid:border-red-600",
+						input?.disabled && "contrast-75",
+						input?.className
+					)}
+					ref={ref}
+				/>
+			</div>
+		);
+	}
+);
+
+export default Input;
